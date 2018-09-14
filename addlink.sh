c337292233da08108c077fd398dfaf5166e624f6
WaYungGit <377209368@qq.com>
2018-09-14 18:05:29 +0800

    modify

--- addlink.sh
+++ addlink.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
 api=http://openapi.eos.ren
 cleos -u $api push action eosshishicai addlink '["'$1'","wayunggogogo"]' -p $1
--- autogetitecoin.sh
+++ autogetitecoin.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-
 n=0
-cnt=4320
-for ((i=1; i<=20; i ++))
+cnt=2410
+for i in $(seq 1 20)
 do
     if [ $n -eq 5 ]; then
     n=0
-    cnt=$cnt+5
+    cnt=$((cnt+5))
     fi
-    let cnt++
-    let n++
-    echo $cnt
+    cnt=$((cnt+1))
+    n=$((n+1))
+    echo $cnt $n
     sh getitecoin.sh robotnum$cnt
     sh addlink.sh robotnum$cnt
 done
